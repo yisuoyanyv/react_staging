18dbc03903f71d84274aedb03bff63c0f805f817
yisuoyanyv <840834298@qq.com>
2021-07-18 16:57:26 +0800

    React init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// //引入react核心库
+// import React from 'react'
+// //引入ReactDOM
+// import ReactDOM from 'react-dom'
+// //引入APP组件
+// import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// //渲染APP到页面
+// ReactDOM.render(<App />,document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React,{Component} from './module.js'
+console.log(React)
+// const {Component} = React
+
+console.log(new Component())
---
+++ src/module.js
@@ -0,0 +1,10 @@
+const React = {a:1,b:2}
+
+export class Component{
+
+}
+
+React.Component = Component
+
+
+export default React
