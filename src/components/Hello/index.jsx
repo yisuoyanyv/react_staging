69591a4cadfc72e596555e739e72885e5b3e785c
yisuoyanyv <840834298@qq.com>
2021-07-18 17:23:23 +0800

    样式的模块化写法

--- src/components/Hello/index.jsx
+++ src/components/Hello/index.jsx
@@ -1,8 +1,8 @@
 import React,{Component} from 'react'
-import './index.css'
+import hello from './index.module.css'
 
 export default class Hello extends Component{
     render(){
-        return <h2 className="title">Hello,React!</h2>
+        return <h2 className={hello.title}>Hello,React!</h2>
     }
 }
