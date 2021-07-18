fae56228b3a4b7d08dd81403ab1d56daa07bc386
yisuoyanyv <840834298@qq.com>
2021-07-18 17:18:23 +0800

    模块化，命名风格

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 //创建“外壳”组件
 import React,{Component} from 'react'
+import Hello from './components/Hello'
+import Welcome from './components/Welcome'
 
-class App extends Component{
+//创建并暴露App组件
+export default class App extends Component{
     render(){
         return (
             <div>
-                hello,react!
+               <Hello />
+               <Welcome />
             </div>
         )
     }
 }
 
-//暴露App组件
-export default App
+
