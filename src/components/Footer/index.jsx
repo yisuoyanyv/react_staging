79f2e55be3dfc40c6d6a6a31919d763e7ff87427
yisuoyanyv <840834298@qq.com>
2021-07-19 23:51:10 +0800

    todoList案例

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,16 +2,33 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Footer extends Component {
+
+    //全选checkBox的回调
+    handleCheckAll=(event)=>{
+        this.props.checkAllTodo(event.target.checked)
+    }
+
+    //清除所有已完成任务的回调
+    handleClearAllDone=()=>{
+        this.props.clearAllDone()
+    }
+
+
     render() {
+        const {todos} =this.props
+        //已完成的个数
+        const doneCount = todos.reduce((pre,todo)=> pre + (todo.done? 1 : 0),0)
+        //总数
+        const total = todos.length
         return (
             <div className="todo-footer">
                 <label>
-                <input type="checkbox"/>
+                <input  type="checkbox" onChange={this.handleCheckAll} checked={doneCount === total && total !== 0 ? true : false}/>
                 </label>
                 <span>
-                <span>已完成0</span> / 全部2
+                <span>已完成{doneCount}</span> / 全部{total}
                 </span>
-                <button className="btn btn-danger">清除已完成任务</button>
+                <button onClick={this.handleClearAllDone} className="btn btn-danger">清除已完成任务</button>
             </div>
         )
     }
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react'
 import './index.css'
 export default class Item extends Component {
+
+    state = {mouse:false} //标识鼠标移入、移出
+
+    handleMouse = (flag)=>{
+        return ()=>{
+            this.setState({mouse:flag})
+        }
+        
+    }
+
+    //勾选、取消勾选某一个todo的回调
+    handleCheck = (id)=>{
+        return (event)=>{
+            this.props.updateTodo(id,event.target.checked)
+        }
+    }
+
+    //删除某一个todo的回调
+    handleDelete = (id)=>{
+        if(window.confirm('确定删除吗？')){
+            this.props.deleteTodo(id)
+        }
+       
+    }
     render() {
+        const {id,name,done} = this.props
+        const {mouse} = this.state
         return (
-            <li>
+            <li style={{backgroundColor:mouse?'#ddd':'white'}} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)}>
                 <label>
-                    <input type="checkbox"/>
-                    <span>yyyy</span>
+                    <input   type="checkbox" checked={done} onChange={this.handleCheck(id)}/>
+                    <span>{name}</span>
                 </label>
-                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+                <button onClick={()=>{this.handleDelete(id)}}  className="btn btn-danger" style={{display:mouse?'block':'none'}}>删除</button>
             </li>
         )
     }
